e938864b1a538dab9a4e2124b5af4c82a8e9cdb5
Luiz <luizz.ggr@gmail.com>
2022-01-15 12:33:40 -0300

    feat: validate emails with domain part larger then 63 chars

--- src/entities/Email.ts
+++ src/entities/Email.ts
@@ -17,6 +17,11 @@ export class Email {
       return false
     }
 
+    const domainParts = domain.split('.')
+    if (domainParts.some(part => part.length > 63)) {
+      return false
+    }
+
     return true
   }
 }

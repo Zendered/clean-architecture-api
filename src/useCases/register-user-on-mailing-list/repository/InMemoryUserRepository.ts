0c46e99d87ad617be502d542dd6869c3503ae2ad
Luiz <luizz.ggr@gmail.com>
2022-01-15 09:44:45 -0300

    fix: use correct eslint command on lintstaged

--- src/useCases/register-user-on-mailing-list/repository/InMemoryUserRepository.ts
+++ src/useCases/register-user-on-mailing-list/repository/InMemoryUserRepository.ts
@@ -1,39 +1,38 @@
-import { IUserRepository } from "../../ports/userRepository";
-import { IUserData } from "../userData";
+import { IUserRepository } from '../../ports/userRepository'
+import { IUserData } from '../userData'
 
 export class InMemoryUserRepository implements IUserRepository {
+  private repository: IUserData[]
 
-    private repository: IUserData[]
+  constructor (repository: IUserData[]) {
+    this.repository = repository
+  }
 
-    constructor(repository: IUserData[]) {
-        this.repository = repository
+  async add (user: IUserData): Promise<void> {
+    const exists = await this.exists(user)
+    if (!exists) {
+      this.repository.push(user)
     }
+  }
 
-    async add(user: IUserData): Promise<void> {
-        const exists = await this.exists(user)
-        if (!exists) {
-            this.repository.push(user)
-        }
+  async findUserByEmail (email: string): Promise<IUserData> {
+    const user = this.repository.filter((user) => {
+      return user.email === email
+    })
+    if (user.length > 0) {
+      return user[0]
     }
+    return null
+  }
 
-    async findUserByEmail(email: string): Promise<IUserData> {
-        const user = this.repository.filter((user) => {
-            return user.email === email
-        })
-        if (user.length > 0) {
-            return user[0]
-        }
-        return null
-    }
+  async findAllUsers (): Promise<IUserData[]> {
+    return this.repository
+  }
 
-    async findAllUsers(): Promise<IUserData[]> {
-        return this.repository
+  async exists (user: IUserData): Promise<boolean> {
+    if (await this.findUserByEmail(user.email) === null) {
+      return false
     }
-
-    async exists(user: IUserData): Promise<boolean> {
-        if (await this.findUserByEmail(user.email) === null) {
-            return false
-        }
-        return true
-    }
+    return true
+  }
 }
--- src/useCases/register-user-on-mailing-list/repository/inMemoryUserReposity.spec.ts
+++ src/useCases/register-user-on-mailing-list/repository/inMemoryUserReposity.spec.ts
@@ -1,29 +1,28 @@
-import { IUserData } from "../userData"
-import { InMemoryUserRepository } from "./InMemoryUserRepository"
+import { IUserData } from '../userData'
+import { InMemoryUserRepository } from './InMemoryUserRepository'
 
-describe("In memory User repository", () => {
-    test("should return null if user is not found", async () => {
-        const users: IUserData[] = []
-        const sut = new InMemoryUserRepository(users)
-        const user = await sut.findUserByEmail("any@email.com")
-        expect(user).toBeNull()
-    })
+describe('In memory User repository', () => {
+  test('should return null if user is not found', async () => {
+    const users: IUserData[] = []
+    const sut = new InMemoryUserRepository(users)
+    const user = await sut.findUserByEmail('any@email.com')
+    expect(user).toBeNull()
+  })
 
-    test("should return user if it is found in the repository", async () => {
-        const users: IUserData[] = []
-        const name = "any_name"
-        const email = "any@email.com"
-        const sut = new InMemoryUserRepository(users)
-        await sut.add({ name, email })
-        const user = await sut.findUserByEmail("any@email.com")
-        expect(user.name).toBe(name)
+  test('should return user if it is found in the repository', async () => {
+    const users: IUserData[] = []
+    const name = 'any_name'
+    const email = 'any@email.com'
+    const sut = new InMemoryUserRepository(users)
+    await sut.add({ name, email })
+    const user = await sut.findUserByEmail('any@email.com')
+    expect(user.name).toBe(name)
+  })
 
-    })
-
-    test("should return all users in the repository", async () => {
-        const users: IUserData[] = [{ name: "any_name", email: "any@mail.com" }, { name: "second_name", email: "second@mail.com" }]
-        const sut = new InMemoryUserRepository(users)
-        const returnedUsers = sut.findAllUsers()
-        expect((await returnedUsers).length).toBe(2)
-    })
+  test('should return all users in the repository', async () => {
+    const users: IUserData[] = [{ name: 'any_name', email: 'any@mail.com' }, { name: 'second_name', email: 'second@mail.com' }]
+    const sut = new InMemoryUserRepository(users)
+    const returnedUsers = sut.findAllUsers()
+    expect((await returnedUsers).length).toBe(2)
+  })
 })

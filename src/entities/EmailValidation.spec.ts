8d4f1ce744f92241bdd9668e96fc193e13d9aa04
Luiz <luizz.ggr@gmail.com>
2022-01-15 12:33:49 -0300

    test: validate emails with domain part larger then 63 chars

--- src/entities/EmailValidation.spec.ts
+++ src/entities/EmailValidation.spec.ts
@@ -16,17 +16,17 @@ describe("Email validation", () => {
         expect(Email.validate(email)).toBeTruthy()
     })
 
-    test("should not accept strings larger then 320 chars", () => {
+    test("should not accept strings larger than 320 chars", () => {
         const email:string = "l".repeat(64) + "@" + "d".repeat(128) + "." + "d".repeat(127)
         expect(Email.validate(email)).toBeFalsy()
     })
 
-    test("should not accept domain part larger then 255 chars", () => {
+    test("should not accept domain part larger than 255 chars", () => {
         const email:string = "local@" + "d".repeat(128) + "." + "d".repeat(127)
         expect(Email.validate(email)).toBeFalsy()
     })
 
-    test("should not accept local part larger then 64 chars", () => {
+    test("should not accept local part larger than 64 chars", () => {
         const email:string = "l".repeat(65) + "@email.com"
         expect(Email.validate(email)).toBeFalsy()
     })
@@ -38,6 +38,11 @@ describe("Email validation", () => {
 
     test("should not accept empty domain", () => {
         const email:string = "any@"
+        expect(Email.validate(email)).toBeFalsy()
+    })
+
+    test("should not accept domain with part larger than 63 chars", () => {
+        const email:string = "any@" + "d".repeat(64) + ".com"
         expect(Email.validate(email)).toBeFalsy()
     })
 })

af6d8ec05d3ce80f29c399c12011c317d6bee55f
Luiz <luizz.ggr@gmail.com>
2022-01-15 12:24:16 -0300

    test: validate emails with empty local part

--- src/entities/EmailValidation.spec.ts
+++ src/entities/EmailValidation.spec.ts
@@ -11,7 +11,7 @@ describe("Email validation", () => {
         expect(Email.validate(email)).toBeFalsy()
    })
 
-   test("should accept valid e", () => {
+   test("should accept valid email", () => {
         const email:string = "any@email.com"
         expect(Email.validate(email)).toBeTruthy()
     })
@@ -28,6 +28,11 @@ describe("Email validation", () => {
 
     test("should not accept local part larger then 64 chars", () => {
         const email:string = "l".repeat(65) + "@email.com"
+        expect(Email.validate(email)).toBeFalsy()
+    })
+
+    test("should not accept empty local part", () => {
+        const email:string = "@email.com"
         expect(Email.validate(email)).toBeFalsy()
     })
 })

03d426196750b45dcc4d81c20fe4aa54a1efaefb
Luiz <luizz.ggr@gmail.com>
2022-01-15 12:40:54 -0300

    test: validade emails with domain part with invalid domain chars

--- src/entities/EmailValidation.spec.ts
+++ src/entities/EmailValidation.spec.ts
@@ -1,48 +1,53 @@
-import { Email } from "./Email"
+import { Email } from './Email'
 
-describe("Email validation", () => {
-    test("should not accept null string", () => {
-         const email = null
-         expect(Email.validate(email)).toBeFalsy()
-    })
+describe('Email validation', () => {
+  test('should not accept null string', () => {
+    const email = null
+    expect(Email.validate(email)).toBeFalsy()
+  })
 
-    test("should not accept empty string", () => {
-        const email:string = ""
-        expect(Email.validate(email)).toBeFalsy()
-   })
+  test('should not accept empty string', () => {
+    const email:string = ''
+    expect(Email.validate(email)).toBeFalsy()
+  })
 
-   test("should accept valid email", () => {
-        const email:string = "any@email.com"
-        expect(Email.validate(email)).toBeTruthy()
-    })
+  test('should accept valid email', () => {
+    const email:string = 'any@email.com'
+    expect(Email.validate(email)).toBeTruthy()
+  })
 
-    test("should not accept strings larger than 320 chars", () => {
-        const email:string = "l".repeat(64) + "@" + "d".repeat(128) + "." + "d".repeat(127)
-        expect(Email.validate(email)).toBeFalsy()
-    })
+  test('should not accept strings larger than 320 chars', () => {
+    const email:string = 'l'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
 
-    test("should not accept domain part larger than 255 chars", () => {
-        const email:string = "local@" + "d".repeat(128) + "." + "d".repeat(127)
-        expect(Email.validate(email)).toBeFalsy()
-    })
+  test('should not accept domain part larger than 255 chars', () => {
+    const email:string = 'local@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
 
-    test("should not accept local part larger than 64 chars", () => {
-        const email:string = "l".repeat(65) + "@email.com"
-        expect(Email.validate(email)).toBeFalsy()
-    })
+  test('should not accept local part larger than 64 chars', () => {
+    const email:string = 'l'.repeat(65) + '@email.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 
-    test("should not accept empty local part", () => {
-        const email:string = "@email.com"
-        expect(Email.validate(email)).toBeFalsy()
-    })
+  test('should not accept empty local part', () => {
+    const email:string = '@email.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 
-    test("should not accept empty domain", () => {
-        const email:string = "any@"
-        expect(Email.validate(email)).toBeFalsy()
-    })
+  test('should not accept empty domain', () => {
+    const email:string = 'any@'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 
-    test("should not accept domain with part larger than 63 chars", () => {
-        const email:string = "any@" + "d".repeat(64) + ".com"
-        expect(Email.validate(email)).toBeFalsy()
-    })
+  test('should not accept domain with part larger than 63 chars', () => {
+    const email:string = 'any@' + 'd'.repeat(64) + '.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept local part with invalid chars', () => {
+    const email:string = 'any email@.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })

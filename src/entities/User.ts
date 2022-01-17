4c4555ec0f66c54c17f45b9ca0c2878c6c6659c5
Luiz <luizz.ggr@gmail.com>
2022-01-17 17:00:05 -0300

    feat: improve error classes

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -17,12 +17,12 @@ export class User {
     static create (userData: IUserData): Either<InvalidNameError | InvalidEmailError, User> {
       const nameOrError = Name.create(userData.name)
       if (nameOrError.isLeft()) {
-        return left(new InvalidNameError())
+        return left(new InvalidNameError(userData.name))
       }
 
       const emailORError = Email.create(userData.email)
       if (emailORError.isLeft()) {
-        return left(new InvalidEmailError())
+        return left(new InvalidEmailError(userData.email))
       }
 
       const name: Name = nameOrError.value as Name
--- src/entities/error/InvalidNameError.ts
+++ src/entities/error/InvalidNameError.ts
@@ -1,1 +1,6 @@
-export class InvalidNameError extends Error {}
+export class InvalidNameError extends Error {
+    public readonly name = 'InvalidNameError'
+    constructor (name:string) {
+      super('Invalid name:' + name + '.')
+    }
+}

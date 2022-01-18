b955c289f87747ae963a1f243d8d72a702f44c71
Luiz <luizz.ggr@gmail.com>
2022-01-18 19:01:34 -0300

    chore: add index.ts for each folder

---
+++ src/entities/error/index.ts
@@ -0,0 +1,2 @@
+export * from './InvalidEmailError'
+export * from './InvalidNameError'
---
+++ src/entities/index.ts
@@ -0,0 +1,4 @@
+export * from './Email'
+export * from './Name'
+export * from './User'
+export * from './userData'

942bd6ea16ffc5342fb6da2b1b4c1e9c92863e7f
Luiz <luizz.ggr@gmail.com>
2022-01-24 10:53:10 -0300

    refactor: intruduce interface IUseCase

--- src/useCases/register-user-on-mailing-list/RegisterUserOnMailingList.ts
+++ src/useCases/register-user-on-mailing-list/RegisterUserOnMailingList.ts
@@ -2,15 +2,16 @@ import { InvalidEmailError, InvalidNameError } from '@src/entities/error/'
 import { User, IUserData } from '@src/entities/'
 import { Either, left, right } from '@src/shared/'
 import { IUserRepository } from './ports/'
+import { IUseCase } from '@src/useCases/register-user-on-mailing-list/ports/'
 
-export class RegisterUserOnMailingList {
+export class RegisterUserOnMailingList implements IUseCase {
     private readonly userRepo: IUserRepository
 
     constructor (userRepo: IUserRepository) {
       this.userRepo = userRepo
     }
 
-    public async registerUserOnMailingList (request: IUserData): Promise<Either<InvalidNameError | InvalidEmailError, IUserData>> {
+    public async perform (request: IUserData): Promise<Either<InvalidNameError | InvalidEmailError, IUserData>> {
       const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
       if (userOrError.isLeft()) {
         return left(userOrError.value)
--- src/useCases/register-user-on-mailing-list/ports/index.ts
+++ src/useCases/register-user-on-mailing-list/ports/index.ts
@@ -1,1 +1,2 @@
 export * from './userRepository'
+export * from './IUseCase'

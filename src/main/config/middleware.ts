87f558c20fd754e514b2ad0bd04a19fe2978ee7c
Luiz <luizz.ggr@gmail.com>
2022-01-24 15:27:09 -0300

    feat: apply content-type middleware

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser } from '@src/main/config/middleware/body-parser'
 import { cors } from '@src/main/config/middleware/cors'
+import { contentType } from './middleware/content-type'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }

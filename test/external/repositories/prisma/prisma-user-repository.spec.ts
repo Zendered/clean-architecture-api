c450e2ba4f815df684374243b7fff6476988a424
Luiz <luizz.ggr@gmail.com>
2022-01-26 19:43:15 -0300

    test: find all users in a prisma repository

--- test/external/repositories/prisma/prisma-user-repository.spec.ts
+++ test/external/repositories/prisma/prisma-user-repository.spec.ts
@@ -22,4 +22,19 @@ describe('Prisma user repository', () => {
     await userRepository.add(user)
     expect(await userRepository.exists(user)).toBeTruthy()
   })
+
+  test('Find all users should return all added users', async () => {
+    const userRepository = new PrismaUserRepository(prisma)
+    await userRepository.add({
+        name: 'any_name',
+        email: 'any@mail.com'
+    })
+    await userRepository.add({
+        name: 'second_name',
+        email: 'second@mail.com'
+    })
+    const users = await userRepository.findAllUsers()    
+    expect(users[0].name).toEqual('any_name')
+    expect(users[1].name).toEqual('second_name')
+  })
 })
